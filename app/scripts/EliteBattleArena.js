8514aeea60db3c91970f519f437d8fcc3aa77e36
unknown <daniel@danielstern.ca>
2014-10-12 14:19:43 -0400

    workingsimulator

---
+++ app/scripts/EliteBattleArena.js
@@ -0,0 +1,112 @@
+angular.module("EliteBattleArena", [])
+
+    .factory("Game", function() {
+        return function() {
+            // console.log("Creating game");
+
+            var game = {
+                actors: [],
+                currentTurn: 0,
+                maxTurns: 1000
+            };
+
+            this.addActor = function(actor) {
+                game.actors.push(actor);
+            }
+
+            this.winConditions = [function(game) {
+                return game.actors.filter(function(actor) {
+                    return actor.side === "evil";
+                }).every(function(villain) {
+                    return villain.health <= 0;
+                });
+            }];
+
+            this.loseConditions = [function(game) {
+                return game.actors.filter(function(actor) {
+                    return actor.side === "good";
+                }).every(function(hero) {
+                    return hero.health <= 0;
+                });
+            }];
+
+
+            this.start = function() {
+                var battleLog = [];
+                while (game.currentTurn < game.maxTurns) {
+
+                    // START OF TURN PHASE
+                    game.currentTurn++;
+
+                    var log = {
+                        turn:game.currentTurn,
+                        actions:[],
+                        narrative:[]
+                    }
+                    // console.log("a turn passes");
+
+                    // AI PHASE
+                    // action "attack, defend, heal, do nothing"
+                    var actions = game.actors.map(function(actor) {
+                        if (actor.health <= 0) return;
+                        return actor.act(game, actor);
+                    })
+
+                    log.actions = actions;
+
+                    // ACTION PHASE
+                    actions.forEach(function(action){
+                        if (action.action === 'attack') {
+                            action.actor.defending = false;
+                            action.target.health -= action.target.defending ? Math.floor(action.actor.attack / 2) : action.actor.attack;
+                            log.narrative.push(action.actor.name + " attacked " + action.target.name + " for " + action.actor.attack + " damage.");
+                        }
+
+                        if (action.action === 'nothing' || undefined) {
+                            log.narrative.push(action.actor.name + " did nothing.");
+                        }
+
+                        if (action.action === 'heal' || undefined) {
+                            action.actor.defending = false;
+                            action.target.health += 5;
+                            log.narrative.push(action.actor.name + " healed " + action.target.name + " for " + action.actor.attack + " damage.");
+                        }
+
+                        if (action.action === 'defend') {
+                            action.target.defending = true;
+                            log.narrative.push(action.actor.name + " is defending " + action.target.name + ".");
+                        }
+
+                        // if (acti)
+                    })
+
+                    // END OF TURN PHASE
+                    log.game = angular.copy(game);
+                    battleLog.push(log);
+
+                    // WIN CONDITION PHASE
+                    var losing = this.loseConditions.some(function(condition) {
+                        return condition(game);
+                    });
+
+                    var winning = this.winConditions.some(function(condition) {
+                        return condition(game);
+                    });
+
+                    if (losing) {
+                        log.narrative.push("The forces of evil have overwhelmed us!");
+                        console.warn("You lost!",game);
+                        break;
+                    }
+
+                    if (winning) {
+                        log.narrative.push("The forces of good have prevailed!");
+                        console.info("You win!", game);
+                        break;
+                    }
+                }
+
+                return battleLog;
+            }
+        }
+    });
---
+++ app/scripts/conditions.js
@@ -0,0 +1,96 @@
+angular.module("EliteBattleArena")
+
+.value("conditions", {
+    isWeak: function(actors) {
+        var targets = actors.filter(function(enemy) {
+            if (enemy.health < enemy.maxHealth / 10) {
+                return true;
+            }
+        });
+        return targets;
+    },
+    isEvil: function(actors) {
+        return actors.filter(function(actor) {
+            return actor.side === "evil";
+        });
+    },
+    isGood: function(actors) {
+        return actors.filter(function(actor) {
+            return actor.side === "good";
+        });
+    },
+    isAlive: function(actors) {
+        return actors.filter(function(actor) {
+            return actor.health > 0;
+        });
+    },
+})
+
+.service("getRandomMove", function(conditions) {
+        return function(game, self) {
+
+            var targets = game.actors;
+            targets = self.side === 'good' ? conditions.isEvil(targets) : conditions.isGood(targets);
+            targets = conditions.isAlive(targets);
+            var target = targets[0];
+
+            if (!target) {
+                console.error(self.name + " can't get target...",game);
+                // return;
+            }
+
+            switch (Math.floor(Math.random() * 4)) {
+                case 0:
+                    return {
+                        action: "nothing",
+                        target: undefined,
+                        actor: this,
+                    };
+
+                case 1:
+                    return {
+                        action: "heal",
+                        target: this,
+                        actor: this,
+                    }
+                case 2:
+                    if (target) {
+                        return {
+                            action: "attack",
+                            target: target,
+                            actor: this,
+                        }
+                    };
+                case 3:
+                    return {
+                        action: "defend",
+                        target: this,
+                        actor: this,
+                    }
+            }
+        }
+    })
+
+    .factory("Actor", function(conditions,getRandomMove) {
+
+
+        return function(options) {
+
+            options = options || {};
+
+            this.id = Math.ceil(Math.random() * 100000),
+            this.name = options.name || this.id;
+            this.act = getRandomMove;
+            this.side = options.side;
+            this.health = options.health || 100;
+            this.attack = options.attack || 10;
+            this.defending = false;
+
+
+        }
+    })
+    .filter('slice', function() {
+      return function(arr, start, end) {
+        return (arr || []).slice(start, end);
+      };
+    });
