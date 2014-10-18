49bd69d763c99ef780cf954f3c0448e6df5428ff
unknown <daniel@danielstern.ca>
2014-10-17 20:09:07 -0400

    what

--- app/modules/EliteBattleArena.Main.Bootstrap.js
+++ app/modules/EliteBattleArena.Main.Bootstrap.js
@@ -1,6 +1,13 @@
 angular.module("Game.EliteBattleArena", ['EliteBattleArena', 'EliteBattleArena.Scenario', 'EliteBattleArena.Sounds', 'ui.router', 'ngAudio'])
     .run(function($rootScope, Game, Actor, armor, weapons, enemiesMap) {
-        var game = localStorage['game'] ? angular.fromJson(localStorage['game']) : new Game();
+        var game;
+        if (localStorage['game']) {
+            game = angular.fromJson(localStorage['game']);
+            initGame(game);
+        } else {
+            game = new Game();
+            initGame(game);
+        }
         console.log("game?", game);
         $rootScope.game = game;
 
@@ -11,57 +18,66 @@ angular.module("Game.EliteBattleArena", ['EliteBattleArena', 'EliteBattleArena.S
                 game = new Game();
                 initGame(game);
             }
-            localStorage['game'] = angular.toJson(game);
+            localStorage['game'] = JSON.stringify(game);
             // console.log("Game json?")
         }, true);
         var TEST_MODE = true;
 
         function initGame(game) {
 
+                if (game.party[0]) {
+                    var hero = game.party[0];
 
-            var hero = new Actor({
-                name: "Friendus Fortunato",
-                side: "good",
-                body: "hero",
-                speed: 6,
-                defense: 2
-            });
+                    var newActor = new Actor();
+                    hero.equipItem = newActor.equipItem.bind(hero);
 
-            if (TEST_MODE) {
-                game.inventory.push(armor.revitalizingPlate);
-                game.inventory.push(weapons.levantineSword);
-                game.maxDungeonLevel = 10;
-            }
+                } else {
 
-            game.party.push(hero);
-            // game.totalDungeonLevels = Object.keys(enemiesMap).length;
-            game.totalDungeonLevels = 9;
 
-            game.store = {
-                weapons: {
-
-                },
-                armor: {
+                    var hero = new Actor({
+                        name: "Friendus Fortunato",
+                        side: "good",
+                        body: "hero",
+                        speed: 6,
+                        defense: 2
+                    });
+                    game.party.push(hero);
 
                 }
-            };
-
-            for (var key in weapons) {
-                if (weapons[key].canBuy) {
-                    game.store.weapons[key] = weapons[key];
+                if (TEST_MODE) {
+                    game.inventory.push(armor.revitalizingPlate);
+                    game.inventory.push(weapons.levantineSword);
+                    game.maxDungeonLevel = 10;
                 }
-            }
 
-            for (var key in armor) {
-                if (armor[key].canBuy) {
-                    game.store.armor[key] = armor[key];
+                // game.totalDungeonLevels = Object.keys(enemiesMap).length;
+                game.totalDungeonLevels = 9;
+
+                game.store = {
+                    weapons: {
+
+                    },
+                    armor: {
+
+                    }
+                };
+
+                for (var key in weapons) {
+                    if (weapons[key].canBuy) {
+                        game.store.weapons[key] = weapons[key];
+                    }
                 }
+
+                for (var key in armor) {
+                    if (armor[key].canBuy) {
+                        game.store.armor[key] = armor[key];
+                    }
+                }
+
+                game.gold = 100;
+                console.log("initinggame;")
             }
-
-            game.gold = 100;
-        }
-
-        initGame(game);
+            // initGame(game);
 
     })
     .directive("button", function(interfaceSounds) {
